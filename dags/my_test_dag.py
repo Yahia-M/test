eb3caa5d05e9bd0714306aa8638a15c9b0520c2b
Yahia M <yhmaamar@gmail.com>
2023-03-11 19:41:44 +0000

    DAGS

---
+++ dags/my_test_dag.py
@@ -0,0 +1,20 @@
+import json
+import pathlib
+
+import airflow
+import requests
+import requests.exceptions as requests_exceptions
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+dag = DAG(
+    dag_id='download_rocket_launches',
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval=None,
+)
+
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command=""
+)
